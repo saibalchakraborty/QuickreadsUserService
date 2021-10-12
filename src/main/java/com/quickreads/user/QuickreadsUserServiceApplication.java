98d860135b29f2e677ce97c3552356d890c4f5a8
saibalchakraborty <saibal183@gmail.com>
2021-10-13 00:35:23 +0530

    setting up project in master

---
+++ src/main/java/com/quickreads/user/QuickreadsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.quickreads.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickreadsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickreadsUserServiceApplication.class, args);
+	}
+
+}
