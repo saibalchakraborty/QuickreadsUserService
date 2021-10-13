c125297faffd1b8d8a171e1330ad9e80017fd669
saibalchakraborty <saibal183@gmail.com>
2021-10-13 23:41:40 +0530

    Removing unused import

--- src/main/java/com/quickreads/user/controller/ReaderController.java
+++ src/main/java/com/quickreads/user/controller/ReaderController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
