95d2719587222549249543e100c27da748fcbc73
saibalchakraborty <saibal183@gmail.com>
2021-10-15 16:59:55 +0530

    Disabled csrf

--- src/main/java/com/quickreads/user/config/SecurityConfig.java
+++ src/main/java/com/quickreads/user/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.quickreads.user.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,4 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailService);
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.csrf().disable();
+	}
+	
+	
 }
